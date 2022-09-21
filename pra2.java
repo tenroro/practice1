91ae140d4df9f46bafdc009b771d297ec96106a6
ochiai <y.ochiai.sys22@morijyobi.ac.jp>
2022-09-21 12:35:08 +0900

    演習２
    "Hello World"を100回出力"

--- pra2.java
+++ pra2.java
@@ -1,3 +1,5 @@
- public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+class pra2{       
+public static void main(String[] args) {
+        for(int i=0;i<=100;i++){
+	       System.out.println("Hello World");
+    }}}
